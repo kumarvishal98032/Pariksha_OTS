63a99f09c3267598aafc3b675c2015f08ec0bed1
kumarvishal98032 <kumarvishal475lpu@gmail.com>
2026-06-29 15:04:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let end = new Date(data.end.replace(" ", "T"));
 
     document.body.innerHTML = `
     <div style="text-align:center;padding:100px;">
-        <h1>JEE Chemistry Test</h1>
+        <h1>Pariksha Online Test Platform</h1>
         <h2>⏳ Test has not started yet.</h2>
         <h3>Exam Starts: ${data.start}</h3>
     </div>`;
@@ -30,7 +30,7 @@ if (now > end) {
 
     document.body.innerHTML = `
     <div style="text-align:center;padding:100px;">
-        <h1>JEE Chemistry Test</h1>
+        <h1>Pariksha Online Test Platform</h1>
         <h2>❌ The test has ended.</h2>
     </div>`;
 
@@ -328,7 +328,7 @@ function submitTest(){
                 max-width:900px;
                 margin:auto;">
 
-        <h1>Chemistry Test Result</h1>
+        <h1>Test Result</h1>
 
         <hr>
 
